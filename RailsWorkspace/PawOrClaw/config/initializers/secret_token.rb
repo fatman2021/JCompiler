15c785b4db0ef0a1706eda416622cd469a61e316
Allegra Larche <allegralarche@gmail.com>
2014-02-14 21:20:08 -0500

    Init

---
+++ RailsWorkspace/PawOrClaw/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PawOrClaw::Application.config.secret_key_base = 'c78658b20c6584d10aabde33a1061468e4aa8cbf789f0e3e13beb51e9e6300dc04126327fade25303acb82410d2bfbdaf0536d39e6607d97a81858b8e573a688'
---
+++ RailsWorkspace/PawOrClaw/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PawOrClaw::Application.config.session_store :cookie_store, key: '_PawOrClaw_session'
